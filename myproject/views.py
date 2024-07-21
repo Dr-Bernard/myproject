a5b0ec5e7b5a65852c062bb84bd1552534b2528d
Dr-Bernard <drsunny3030@gmail.com>
2024-07-21 01:45:46 +0100

    Starte customizing the Home page

--- myproject/views.py
+++ myproject/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def homepage(request):
-    return HttpResponse("Welcome to your reliable Telemedicine application")
+    # return HttpResponse("Welcome to MEDTECH, your reliable Telemedicine application")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("About MEDTECH a subsidiary of VENTURE TRIBE")
+    # return HttpResponse("About MEDTECH a subsidiary of VENTURE TRIBE")
+    return render(request, 'about.html')
