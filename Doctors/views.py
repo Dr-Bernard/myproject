33c5c8373ca391b07a6ba7e52affc0fcbd87a945
Dr-Bernard <drsunny3030@gmail.com>
2024-07-27 19:52:03 +0100

    Created Superadmin etc

--- Doctors/views.py
+++ Doctors/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Doctors
 
 # Create your views here.
 def Doctors_list(request):
-    return render(request, 'Doctors/Doctors_list.html')
+    doctors = Doctors.objects.all()
+    return render(request, 'Doctors/Doctors_list.html', { 'Doctors': Doctors })
