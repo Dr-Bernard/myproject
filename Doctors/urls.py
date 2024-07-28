46738c808ea0ac670caa0e090ce8ddd2af0c1e85
Dr-Bernard <drsunny3030@gmail.com>
2024-07-28 17:11:24 +0100

    Added more information to the web application.

--- Doctors/urls.py
+++ Doctors/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.doctors_list),
+    path('', views.doctors_list, name="doctors"),
 ]
--- Doctors/views.py
+++ Doctors/views.py
@@ -3,5 +3,5 @@ from .models import Doctor
 
 # Create your views here.
 def doctors_list(request):
-    doctors = Doctor.objects.all()
+    doctors = Doctor.objects.all().order_by('-date')
     return render(request, 'doctors/doctors_list.html', { 'doctors': doctors })
